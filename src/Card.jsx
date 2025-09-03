7c79e6a7c2e0e84b66ff876a18ee049a0fe1f0cc
DUONG Phu-Hiep <duongphuhiep@gmail.com>
2025-09-03 19:10:21 +0200

    update year 2025

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
 	return (
 		<div class="card">
 			<div><span>NOM de l'enfant:</span><span>DUONG</span></div>
-			<div><span>Class:</span><span>PS1</span></div>
+			<div><span>Class:</span><span>MS5</span></div>
 			<div><span>Date:</span><span>&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/{merged.year}</span></div>
 			<div><span>Cantine:</span><span>OUI</span></div>
 			<div><span>Garderie du soir:</span><span>NON</span></div>
